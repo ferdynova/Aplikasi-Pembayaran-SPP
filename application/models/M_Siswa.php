26a48004adeba72c3008cc97d85f5edd6c78ddbe
unknown <faidmeasi106@gmail.com>
2021-11-22 09:37:45 +0700

    initial commit

---
+++ application/models/M_Siswa.php
@@ -0,0 +1,19 @@
+
+<?php
+class M_Siswa extends CI_Model{
+  function data_siswa(){
+    $query = $this->db->query("select * from siswa");
+    return $query;
+  }
+  function simpan(){
+        $data = array('nisn' => $this->input->post('nisn'),
+                'nis' => ($this->input->post('nis')),
+                'nama' => ($this->input->post('nama')),
+                'id_kelas' => ($this->input->post('id_kelas')),
+                'alamat' => ($this->input->post('alamat')),
+                'no_telp' => ($this->input->post('no_telp')),
+                'id_spp' => $this->input->post('id_spp'));
+        $insert = $this->db->insert('siswa',$data);
+    }
+
+}
